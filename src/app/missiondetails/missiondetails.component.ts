3bf5036aab11914ee00c42260728b909811650d4
Marcq04 <156446124+Marcq04@users.noreply.github.com>
2025-04-02 17:26:02 -0400

    added missiondetails component

--- src/app/missiondetails/missiondetails.component.ts
+++ src/app/missiondetails/missiondetails.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpacexApiService } from '../services/spacex-api.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-missiondetails',
-  standalone: false,
   templateUrl: './missiondetails.component.html',
-  styleUrl: './missiondetails.component.css'
+  styleUrls: ['./missiondetails.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
-export class MissiondetailsComponent {
+export class MissiondetailsComponent implements OnInit {
+  missionDetails: any;
 
+  constructor(private route: ActivatedRoute, private spacexApi: SpacexApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    const flightNumber = this.route.snapshot.paramMap.get('flight_number');
+    if (flightNumber) {
+      this.spacexApi.getLaunches().subscribe(launches => {
+        this.missionDetails = launches.find((launch: { flight_number: number }) => 
+          launch.flight_number == +flightNumber // Ensure flightNumber is compared as a number
+        );
+      });
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
 }
